e1c472a77f2a665708d2af2959e404a87481ef7f
rintu-reji <rintucheruvathoor@gmail.com>
2025-01-28 11:33:47 +0530

    Made changes in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const Header: React.FC = () => {
   return (
     <header className="bg-gray-900 text-white shadow-md py-6">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-3xl font-semibold">Rintu C Reji</h1>
+        {/* <h1 className="text-3xl font-semibold">Rintu C Reji</h1> */}
+        <h1 className="text-3xl font-semibold hidden sm:block">Rintu C Reji</h1>
+        <h1 className="text-2xl font-semibold sm:hidden">Rintu</h1>
         <nav className="space-x-6">
           <a href="#about" className="text-lg hover:text-blue-400">
             About
