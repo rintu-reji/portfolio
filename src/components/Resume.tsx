dfce0d17ebdb2929207672b88ca6912019be836e
rintu-reji <rintucheruvathoor@gmail.com>
2025-01-28 10:59:13 +0530

    Made changes in resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -6,7 +6,7 @@ const Resume: React.FC = () => {
       <h2 className="text-4xl font-semibold text-center mb-8">Resume</h2>
       <div className="text-center">
         <a
-          href="/rintucreji_resume.pdf"
+          href="rintucreji_resume.pdf"
           download
           className="bg-blue-500 text-white py-3 px-6 rounded-full hover:bg-blue-600 transition-all"
         >
